0974994619a6f14d414b5eeaafdcfa80bfb0952e
Diones <pereiradiones987@gmail.com>
2025-05-13 09:19:46 -0300

    controller

--- src/marvel/marvel.controller.ts
+++ src/marvel/marvel.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { MarvelService } from './marvel.service';
 
 @Controller('marvel')
-export class MarvelController {}
+export class MarvelController {
+  constructor(private readonly marvelService: MarvelService) { }
+
+  @Get()
+  async getComics(@Query('limit') limit: number, @Query('offset') offset: number) {
+    return this.marvelService.getComics(limit, offset);
+  }
+}
