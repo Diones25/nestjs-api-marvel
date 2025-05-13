a93151d12d2d44f98971d82003b5ea6ad476258f
Diones <pereiradiones987@gmail.com>
2025-05-13 08:51:55 -0300

    configurando variavéis de ambiente no modulo marvel

--- src/marvel/marvel.module.ts
+++ src/marvel/marvel.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MarvelService } from './marvel.service';
 import { MarvelController } from './marvel.controller';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [HttpModule,
+    ConfigModule.forRoot(),
+  ],
   providers: [MarvelService],
   controllers: [MarvelController]
 })
